16af0eca5c8bb785fdfb8ea84de5520363d8f732
Lubos Tomandl <lubos.tomandl@citrix.com>
2021-12-12 02:42:51 +0100

    set color of star based on state

--- src/components/star-button.js
+++ src/components/star-button.js
@@ -1,11 +1,20 @@
 import React from "react"
 import { IconButton, Tooltip } from "@chakra-ui/react"
 import { StarIcon } from "@chakra-ui/icons"
+import { connect } from "react-redux"
 
-export default function StarButton(launch) {
+function StarButton({ launch, ...props }) {
+  let label = "Add to favorites"
+  let iconColor = "grey"
+
+  if (props.favorites.includes(launch.flight_number)) {
+    label = "Remove from favorites"
+    iconColor = "#FFB233"
+  }
+
   return (
     <Tooltip
-      label="Add to favorites"
+      label={label}
       fontSize="md"
       placement="bottom"
       openDelay={1000}
@@ -15,12 +24,20 @@ export default function StarButton(launch) {
       <IconButton
         size="sm"
         zIndex="5"
-        aria-label="Add to favorites"
+        aria-label={label}
         position="absolute"
         top={3}
         right={3}
-        icon={<StarIcon color="grey" />}
+        icon={<StarIcon color={iconColor} />}
       />
     </Tooltip>
   )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    favorites: state.favorites.map((launch) => launch.flight_number),
+  }
+}
+
+export default connect(mapStateToProps)(StarButton)
