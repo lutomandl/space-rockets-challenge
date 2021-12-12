55e1281e98425d97166d84048eebe7e41c18f031
Lubos Tomandl <lubos.tomandl@citrix.com>
2021-12-12 02:31:31 +0100

    add redux store and reducer

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import { ChakraProvider } from "@chakra-ui/react"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
 
+import favoritesReducer from "./reducers/favoritesReducer"
 import App from "./components/app"
+
+const store = createStore(
+  favoritesReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <ChakraProvider resetCSS={true}>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ChakraProvider>
     </Router>
   </React.StrictMode>,
---
+++ src/reducers/favoritesReducer.js
@@ -0,0 +1,7 @@
+const initState = {
+  favorites: [],
+}
+
+export default function favoritesReducer(state = initState, action) {
+  return state
+}
