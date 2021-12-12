c20b8b3a845f0fd70ac2d6ed2f5e42d88f9b4844
Lubos Tomandl <lubos.tomandl@citrix.com>
2021-12-12 02:52:03 +0100

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -4,21 +4,35 @@ import { BrowserRouter as Router } from "react-router-dom"
 import { ChakraProvider } from "@chakra-ui/react"
 import { createStore } from "redux"
 import { Provider } from "react-redux"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import { PersistGate } from "redux-persist/integration/react"
 
 import favoritesReducer from "./reducers/favoritesReducer"
 import App from "./components/app"
 
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, favoritesReducer)
+
 const store = createStore(
-  favoritesReducer,
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <ChakraProvider resetCSS={true}>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </ChakraProvider>
     </Router>
